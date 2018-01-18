2f86ddeff40520b769a1d262e41cc2c792c37e72
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-17 22:59:29 -0700

    doing a react tutorial

--- reviewing-react/src/Test/Tutorial.js
+++ reviewing-react/src/Test/Tutorial.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react'
+import 
+
+const formatName = (user) => {
+    return user.firstName + ' ' + user.lastName
+}
+
+const user = {
+    firstName: "Aly R.",
+    lastName: "Wayne"
+}
+
