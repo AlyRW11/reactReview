0931217d3b3045412d0794568e6d9a53f38fcaff
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-17 23:11:20 -0700

    fixing React-Dom

--- reviewing-react/src/Test/Tutorial.js
+++ reviewing-react/src/Test/Tutorial.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 class Tutorial extends Comment {
     render (){
-
 const formatName = (user) => {
     return user.firstName + ' ' + user.lastName
 }
@@ -17,12 +16,13 @@ const element = (
     <h1> Hello, {formatName(user)}!</h1>
 )
 
-// ReactDom.render(
-//     element,
-//     document.getElementById('root')
-// )
+ReactDom.render(
+    element,
+    document.getElementById('root')
+)
 
-}}
+}
+}
 
 export default Tutorial
 
