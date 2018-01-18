633e3eaf265073d4950a0d84d3cec66165bbfaaa
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-17 23:09:47 -0700

    Testing embedding expressions

--- reviewing-react/src/App.js
+++ reviewing-react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Testing from './Test/Test'
+import Tutorial from './Test/Tutorial'
 
 class App extends Component {
   render() {
@@ -11,9 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <Testing/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tutorial/>
       </div>
     );
   }
--- reviewing-react/src/Test/Tutorial.js
+++ reviewing-react/src/Test/Tutorial.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import 
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+class Tutorial extends Comment {
+    render (){
 
 const formatName = (user) => {
     return user.firstName + ' ' + user.lastName
@@ -9,4 +12,17 @@ const user = {
     firstName: "Aly R.",
     lastName: "Wayne"
 }
+
+const element = (
+    <h1> Hello, {formatName(user)}!</h1>
+)
+
+// ReactDom.render(
+//     element,
+//     document.getElementById('root')
+// )
+
+}}
+
+export default Tutorial
 
